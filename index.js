095f3a25b71cb84212593c42f030a6bbb3c013a6
gdreamer1 <gdadreamer@gmail.com>
2021-03-20 17:18:09 -0700

    completed assinments

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+// const friends = ['Lisa', 'Kaitlin', 'Jan']
+// const event = "surprise"
+
+function writeCards(friends, event){
+const messages = []
+
+    for (let i = 0; i < friends.length; i++) {
+       const message = `Thank you, ${friends[i]}, for the wonderful ${event} gift!`;
+       messages.push(message);
+    }
+    return messages;
+}
+
+
+ function countDown(number) { 
+ while (number > -1) {
+   console.log(number);
+   number--
+ }
+}
+
+countDown(10)
